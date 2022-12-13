ab37290ed430e697853ce28101e139d5d4647ee9
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-12 22:25:29 -0500

    home page displays restaurants

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,30 +2,38 @@ import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import {BASE_URL} from '../globals'
+import { BASE_URL } from '../globals'
 
 const Home = ({customer, authenticated}) => {
   let navigate = useNavigate()
 
-  const [restaurant, setRestaurant] = useState([])
+  const [restaurantList, setRestaurant] = useState([])
+
+  const GetAllRest = async () => {
+    const res = await axios.get(`${BASE_URL}/restaurants/get_all`)
+    setRestaurant(res.data)
+  }
 
   useEffect(() => {
-    const getAllRest = async () => {
-      const res = await axios.get(`${BASE_URL}/restaurants/get_all}`)
-      console.log('test2', res.data)
-      setRestaurant(res.data)
-    }
-    getAllRest()
+    // const getAllRest = async () => {
+    //   const res = await axios.get(`${BASE_URL}/restaurants/get_all}`)
+    //   console.log('test2', res.data)
+    //   setRestaurant(res.data)
+    // }
+    GetAllRest()
   }, [])
   return (
     <div>Home
       <section className='user-restaurants'> 
-                    <h1 className='text-4xl font-1-bold pb-6 pt-20'>You're restaurant Restaurants</h1>
+                    <h1 className='text-4xl font-1-bold pb-6 pt-20'>Your Restaurants</h1>
                     <div className='trending-container'>
-                            <Link to={`/restaurant/${restaurant?.id}`} key={restaurant.id}>
+                      {restaurantList.map((restaurant) => (
+                        <Link to={`/restaurant/${restaurant?.id}`} key={restaurant.id}>
                                 <img src={restaurant.image} alt={restaurant.name} className="restaurant-img" />
                                 <h5 className='fav-rest-name'>{restaurant.name}</h5>     
                             </Link>
+                      ))}
+                            
                     </div>
                 </section>
     </div>
