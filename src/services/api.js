1297bbe10d56fdc1acce02a9f3664d30b5a9f5dc
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-04 16:13:57 -0500

    base url updated for axios request

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-// export const BASE_URL = ''
-export const BASE_URL = 'http://localhost:3001/api/'
+export const BASE_URL = 'https://door-drop-backend.herokuapp.com/api'
+// export const BASE_URL = 'http://localhost:3001/api/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
