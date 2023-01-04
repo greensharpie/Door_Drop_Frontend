b4816fb595799c6e0ad8272a026984899cdd89d7
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-04 15:38:02 -0500

    updated base url

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3001/api'
-// export const BASE_URL = '/api'
+// export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://door-drop-backend.herokuapp.com/api'
